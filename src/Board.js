7db227365a529ea8f2eaacf389059bcf70e57c81
Andrew Risse <andrewrisse@gmail.com>
2020-01-14 14:47:57 -0500

    initial commit

---
+++ src/Board.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Box from './Box';
+import './Board.css'
+
+class Board extends React.Component{
+    render(){
+        
+        return(
+            <div className='board'>
+                <div className='row'>
+                    <div className='column'>
+                        <Box />
+                    </div>
+                
+                    <div className='column'>
+                        <Box />
+                    </div>
+                    <div className='column'>
+                        <Box />
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='column'>
+                        <Box />
+                    </div>
+                
+                    <div className='column'>
+                        <Box />
+                    </div>
+                    <div className='column'>
+                        <Box />
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='column'>
+                        <Box />
+                    </div>
+                
+                    <div className='column'>
+                        <Box />
+                    </div>
+                    <div className='column'>
+                        <Box />
+                    </div>
+                </div>
+                
+            </div>
+            
+        );
+        
+    }
+}
+
+export default Board;
---
+++ src/Box.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Box.css';
+
+class Box extends React.Component{
+    render(){
+        return(
+            <button className='box' />
+        );
+    }
+}
+
+export default Box;
