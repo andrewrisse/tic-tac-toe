eef2459c50d6b216b5e46b7875f459f23ff258d4
Andrew Risse <andrewrisse@gmail.com>
2020-01-15 09:11:17 -0500

    change run settings in pipeline.yml

--- ticTacBuildScript.sh
+++ ticTacBuildScript.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Hello"
+set -eu
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export fly_target=${fly_target:-tic-tac-toe}
+echo "Concourse target ${fly_target}"
+echo "$(basename $DIR)"
+
