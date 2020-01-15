2c0076ce46ca01c429ba822857d3aab32549ce8d
Andrew Risse <andrewrisse@gmail.com>
2020-01-15 09:32:48 -0500

    change build script to run npm build

--- ticTacBuildScript.sh
+++ ticTacBuildScript.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-set -eu
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export fly_target=${fly_target:-tic-tac-toe}
-echo "Concourse target ${fly_target}"
-echo "$(basename $DIR)"
+npm run build
 
 
